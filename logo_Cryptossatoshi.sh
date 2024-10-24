b2a5b3fc76ee42c54a0365609b6e112d07f46e0d
Cryptossatoshi <andreichpro@gmail.com>
2024-10-24 22:43:42 +0300

    Create logo_Cryptossatoshi.sh

---
+++ logo_Cryptossatoshi.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Логотип текста в ярком циановом цвете
+echo -e '\e[0;32m'  # Устанавливаем яркий циановый цвет текста
+echo -e '                                                                                                         ,,          ,,  '
+echo -e '  .g8"""bgd                                mm                                     mm                   `7MM          db  '
+echo -e '.dP'     `M                                MM                                     MM                     MM              '
+echo -e 'dM'       ``7Mb,od8 `7M'   `MF'`7MMpdMAo.mmMMmm ,pW"Wq.  ,pP"Ybd ,pP"Ybd  ,6"Yb.mmMMmm ,pW"Wq.  ,pP"Ybd  MMpMMMb.  `7MM  '
+echo -e 'MM           MM' "'   VA   ,V    MM   `Wb  MM  6W'   `Wb 8I   `" 8I   `" 8)   MM  MM  6W'   `Wb 8I   `"  MM    MM    MM  '
+echo -e 'MM.          MM        VA ,V     MM    M8  MM  8M     M8 `YMMMa. `YMMMa.  ,pm9MM  MM  8M     M8 `YMMMa.  MM    MM    MM  '
+echo -e '`Mb.     ,'  MM         VVV      MM   ,AP  MM  YA.   ,A9 L.   I8 L.   I8 8M   MM  MM  YA.   ,A9 L.   I8  MM    MM    MM  '
+echo -e '  `"bmmmd' .JMML.       ,V       MMbmmd'   `Mbmo`Ybmd9'  M9mmmP' M9mmmP' `Moo9^Yo.`Mbmo`Ybmd9'  M9mmmP'.JMML  JMML..JMML.'
+echo -e '                       ,V        MM                                                                                      '
+echo -e '                    OOb"       .JMML.                                                                                    '
+echo -e '\e[0m'  # Сбрасываем цвет
