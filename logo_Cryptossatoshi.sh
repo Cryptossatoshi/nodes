6bbf854e2c097bdaf58758a9c0cb6e78dd3aa833
Cryptossatoshi <andreichpro@gmail.com>
2024-11-06 00:55:59 +0300

    logo_Cryptossatoshi.sh

--- logo_Cryptossatoshi.sh
+++ logo_Cryptossatoshi.sh
@@ -2,14 +2,12 @@
 
 # Логотип текста в ярком циановом цвете
 echo -e '\e[0;32m'  # Устанавливаем яркий циановый цвет текста
-echo -e '                                                                                                         ,,          ,,  '
-echo -e '  .g8"""bgd                                mm                                     mm                   `7MM          db  '
-echo -e '.dP'     `M                                MM                                     MM                     MM              '
-echo -e 'dM'       ``7Mb,od8 `7M'   `MF'`7MMpdMAo.mmMMmm ,pW"Wq.  ,pP"Ybd ,pP"Ybd  ,6"Yb.mmMMmm ,pW"Wq.  ,pP"Ybd  MMpMMMb.  `7MM  '
-echo -e 'MM           MM' "'   VA   ,V    MM   `Wb  MM  6W'   `Wb 8I   `" 8I   `" 8)   MM  MM  6W'   `Wb 8I   `"  MM    MM    MM  '
-echo -e 'MM.          MM        VA ,V     MM    M8  MM  8M     M8 `YMMMa. `YMMMa.  ,pm9MM  MM  8M     M8 `YMMMa.  MM    MM    MM  '
-echo -e '`Mb.     ,'  MM         VVV      MM   ,AP  MM  YA.   ,A9 L.   I8 L.   I8 8M   MM  MM  YA.   ,A9 L.   I8  MM    MM    MM  '
-echo -e '  `"bmmmd' .JMML.       ,V       MMbmmd'   `Mbmo`Ybmd9'  M9mmmP' M9mmmP' `Moo9^Yo.`Mbmo`Ybmd9'  M9mmmP'.JMML  JMML..JMML.'
-echo -e '                       ,V        MM                                                                                      '
-echo -e '                    OOb"       .JMML.                                                                                    '
+echo -e ' _____                  _                      _            _     _ '
+echo -e '/ ____|                | |                    | |          | |   (_)'
+echo -e '| |     _ __ _   _ _ __ | |_ ___  ___ ___  __ _| |_ ___  ___| |__  _ '
+echo -e '| |    | '__| | | | '_ \| __/ _ \/ __/ __|/ _` | __/ _ \/ __| '_ \| |'
+echo -e '| |____| |  | |_| | |_) | || (_) \__ \__ \ (_| | || (_) \__ \ | | | |'
+echo -e ' \_____|_|   \__, | .__/ \__\___/|___/___/\__,_|\__\___/|___/_| |_|_| '
+echo -e '              __/ | |                                                '
+echo -e '             |___/|_|                                                '
 echo -e '\e[0m'  # Сбрасываем цвет
